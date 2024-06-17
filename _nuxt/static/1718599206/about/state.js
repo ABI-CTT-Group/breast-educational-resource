89c65768d06a5a0605210ffbf30d555c2763fe71
LinkunGao <gaolinkun123@gmail.com>
2024-06-17 09:48:14 +0000

    deploy: 7445e5223e420065d07e025ee9a58cb3a507266f

--- _nuxt/static/1718599206/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbreast-educational-resource\u002F_nuxt\u002Fstatic\u002F1718599206",layout:"default",error:a,state:{currentContent:{}},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fbreast-educational-resource\u002F",assetsPath:"\u002Fbreast-educational-resource\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1718617690/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fmodel-breast","\u002Fabout","\u002Fvideo"]})
