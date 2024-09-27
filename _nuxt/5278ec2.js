ee50f38b066229ab53922a624ac97ed588f9f3dd
LinkunGao <gaolinkun123@gmail.com>
2024-09-27 09:43:53 +0000

    deploy: bbaf6156f1f01e5bb06e1eb78ab17926a8e52972

--- _nuxt/5278ec2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{565:function(e,n,t){"use strict";t.r(n);var r={middleware:function(e){return(0,e.redirect)("/model-breast")}},l=t(34),component=Object(l.a)(r,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
