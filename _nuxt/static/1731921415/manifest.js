5e45e4f88fce8be8c52a8c1496cc44580b8ef38e
LinkunGao <gaolinkun123@gmail.com>
2024-11-20 20:49:12 +0000

    deploy: df59fa06522c082ba207d168e4b4b0bd9b16e186

--- _nuxt/static/1731921415/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcancer-ductal","\u002Fmodel-breast","\u002Fabout","\u002Fdensity-1","\u002Fdensity-4","\u002Fvideo","\u002Fbenign-cyst","\u002Fbenign-fibroadenoma","\u002Fdensity-3","\u002Fcancer-lobular","\u002Fdensity-2","\u002Fcancer-dcis"]})
